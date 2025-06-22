d261da4f615eb94822bb2f604e47f96272c44062
niki <nikola.stanic.official@gmail.com>
2025-06-22 13:50:11 +0200

    Initial

---
+++ src/components/Navigation/navigationItems.ts
@@ -0,0 +1,10 @@
+import type {NavigationItem} from './types';
+
+export const navigationItems: NavigationItem[] = [
+    {label: 'HOME', path: '/'},
+    {label: 'ABOUT', path: '#about'},
+    {label: 'PROJECTS', path: '#projects'},
+    {label: 'SKILLS', path: '#skills'},
+    {label: 'RESUME', path: '#resume'},
+    {label: 'CONTACT', path: '#contact'},
+];
